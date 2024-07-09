6504b9631e8c5b3e4fc8b11122a986a3a1a51b17
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2024-07-09 14:40:24 +0000

    aula 1 - minuto 50

---
+++ src/lib/mail.ts
@@ -0,0 +1,6 @@
+import nodemailer from 'nodemailer'
+
+
+async function getMailClient() {
+    const account = await nodemailer.createTestAccount
+}
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+import { createTrip } from './routes/create-trips'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+
+
+const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
+
+app.listen({port: 3333}).then(() => {
+    console.log('server unning')
+
+})
