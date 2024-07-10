722be0c3fd5b3886607656646b4869a6ab7efe11
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2024-07-10 00:03:13 +0000

    final aula 1

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,6 +1,18 @@
 import nodemailer from 'nodemailer'
 
 
-async function getMailClient() {
-    const account = await nodemailer.createTestAccount
+export async function getMailClient() {
+    const account = await nodemailer.createTestAccount()
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    })
+
+    return transporter
 }
--- src/routes/create-trips.ts
+++ src/routes/create-trips.ts
@@ -3,6 +3,8 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import {date, z} from 'zod'
 import { prisma } from "../lib/prisma";
 import dayjs from "dayjs";
+import { getMailClient } from "../lib/mail";
+import nodemailer from 'nodemailer'
 
 export async function createTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -12,11 +14,12 @@ export async function createTrip(app: FastifyInstance) {
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
-                owner_email: z.string().email()
+                owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email())
             })
         }
     } ,async(request) => {
-        const {destination, ends_at, starts_at, owner_name, owner_email} = request.body
+        const {destination, ends_at, starts_at, owner_name, owner_email, emails_to_invite} = request.body
 
         if(dayjs(starts_at).isBefore(new Date())){
             throw new Error ('invalid trip start date')
@@ -30,9 +33,44 @@ export async function createTrip(app: FastifyInstance) {
             data: {
                 destination,
                 starts_at,
-                ends_at
+                ends_at,
+                participants: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true
+                            },
+                            ...emails_to_invite.map(email => {
+                                return {email}
+                            })
+                            
+                        ],
+
+
+                    }
+                }
             }
         })
+
+        const mail = await getMailClient()
+
+        const message = await mail.sendMail({
+            from: {
+                name: 'Equipe planner',
+                address: 'wagner.gomes@gmail.com'
+            },
+            to: {
+                name: owner_name,
+                address: owner_email           
+            },
+            subject: 'testando envio do email',
+            html: '<p>teste</p>'
+        })
+
+        console.log(nodemailer.getTestMessageUrl(message))
 
         return {
             tripId: trip.id
